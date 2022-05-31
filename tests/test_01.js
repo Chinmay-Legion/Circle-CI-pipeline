5243e886482b492ff82b1c12e8717a285c198c27
Mindfire-Chinmay <chinmay.singh@mindfiresolutions.com>
2022-06-01 03:54:17 +0530

    new

--- tests/test_01.js
+++ tests/test_01.js
@@ -1,9 +1,8 @@
-
 import { Selector } from "testcafe";
 
-fixture`Valid login`.page`https://opensource-demo.orangehrmlive.com/`;
+fixture`User Login with valid credentials`.page`https://opensource-demo.orangehrmlive.com/`;
 
-test("User Login with valid credentials", async (t) => {
+test("Valid Login", async (t) => {
   await t.maximizeWindow();
   const productVersion = Selector("#footer > div:nth-child(1)");
   const loginName = Selector("#divUsername");
@@ -11,10 +10,31 @@ test("User Login with valid credentials", async (t) => {
   const submitButton = Selector("#btnLogin");
   const dashboard = Selector("#content > div > div.head > h1");
 
-  await t.expect(productVersion.innerText).contains("OrangeHRM 4");
-  await t.typeText(loginName, "Admin", { paste: true });
-  await t.typeText(password, "admin123", { paste: true });
-  await t.click(submitButton);
-  await t.expect(dashboard.exists).ok();
-  await t.expect(dashboard.innerText).contains("Dashboard");
+  await t
+  .expect(productVersion.innerText).contains("OrangeHRM 4")
+  .typeText(loginName, "Admin", { paste: true })
+  .typeText(password, "admin123", { paste: true })
+  .click(submitButton)
+  .expect(dashboard.exists).ok()
+  .expect(dashboard.innerText).contains("Dashboard")
+});
+
+
+test("Invalid Login", async (t) => {
+  await t.maximizeWindow();
+  const productVersion = Selector("#footer > div:nth-child(1)");
+  const loginName = Selector("#divUsername");
+  const password = Selector("#txtPassword");
+  const submitButton = Selector("#btnLogin");
+  const dashboard = Selector("#content > div > div.head > h1")
+  const InvalidCred = Selector('span#spanMessage');
+
+  await t
+  .expect(productVersion.innerText).contains("OrangeHRM 4")
+  .typeText(loginName, "Admin", { paste: true })
+  .typeText(password, "IncorrectPassword123", { paste: true })
+  .click(submitButton)
+  .expect(InvalidCred.innerText).match(/Invalid credentials/i)
+  .expect(dashboard.exists).notOk();
+  
 });
